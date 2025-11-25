31b13a0c91a5bf590e6be9db883f28db301b6935
Lionel-Villanueva <salim.villanueva@tecsup.edu.pe>
2025-11-25 12:04:11 -0500

    Nivel 1 animaciones y plataformas

---
+++ Assets/ScriptableObjects/Enemys.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemys", menuName = "Scriptable Objects/Enemys")]
+public class Enemys : ScriptableObject
+{
+    
+}
--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -4,17 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 6f;
-    public float jumpForce = 6f;
+    public float jumpForce = 12f;
 
     private Rigidbody2D rb;
     private PlayerController inputs;
 
+    public Animator animator;
+    
     private float horizontal;
     private bool jumpRequested;
 
     [Header("Ground Detection")]
     public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
+    public float groundCheckRadius = 0.1f;
     public LayerMask groundLayer;
 
     private void Awake()
@@ -33,10 +35,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Movimiento lateral
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        animator.SetFloat("Movement", Mathf.Abs(horizontal));
+        if (rb.linearVelocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (rb.linearVelocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
 
-        // Salto (solo si está tocando suelo)
         if (jumpRequested && IsGrounded())
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
@@ -49,4 +58,5 @@ public class PlayerMovement : MonoBehaviour
     {
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
+    
 }
